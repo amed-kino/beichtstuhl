95fb4793e7f475f5a5e10df60151e470d760011a
Amed Kino <amed@xbyte.team>
2017-08-17 16:47:41 +0200

    Update record .sh

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,4 +1,4 @@
 TIME_NOW=$(date +%s%N)
 # ffmpeg -f v4l2 -i /dev/video0 -f alsa -ac 2 -i hw:0 "outputs/video_$TIME_NOW.mkv" -y
 
-ffmpeg -y -f alsa -ac 2 -i pulse -acodec pcm_s16le -f v4l2 -framerate 25 -i /dev/video0 "outputs/video_$TIME_NOW.mkv"
+ffmpeg -y -f alsa -ac 2 -i pulse -f v4l2 -i /dev/video0 "outputs/video_$TIME_NOW.mkv"
