bc9a02957d6d16605a24817910136f3e983af5da
Amed Kino <amed@xbyte.team>
2017-08-17 15:31:24 +0200

    Mirror filter enhance

--- scripts/mirror_filter1.sh
+++ scripts/mirror_filter1.sh
@@ -1,1 +1,1 @@
-mplayer -fs tv:// -tv driver=v4l2:device=/dev/video0 -xineramascreen 1 -vf mirror,smartblur=.6:-.5:0,unsharp=l5x5:.8:c5x5:.4,hue=-20:5,gradfun=130:220
+mplayer -fs tv:// -tv driver=v4l2:device=/dev/video1 -xineramascreen 1 -vf mirror,unsharp=l12x5:.8:c8x8:8,hue=-5:10,eq2=8:7:4:4:122
