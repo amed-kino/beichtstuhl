2e9004cec1a1978340ad65ec0056b5b161d9d31b
Amed Kino <amed@xbyte.team>
2017-08-17 16:24:28 +0200

    Fixing record channel

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,2 +1,2 @@
 TIME_NOW=$(date +%s%N)
-ffmpeg -f v4l2 -i /dev/video1 -f alsa -ac 2 -i hw:1 "outputs/video_$TIME_NOW.mkv" -y
+ffmpeg -f v4l2 -i /dev/video0 -f alsa -ac 2 -i hw:0 "outputs/video_$TIME_NOW.mkv" -y
